e878ad736aa0189a843a9a631411598644af8cfb
Chris Olido <chris.olido@gmail.com>
2021-09-15 16:59:07 +0800

    Update WikipediaHomePage.java
    change to https://dmuat.lta.gov.sg

--- src/test/java/starter/navigation/WikipediaHomePage.java
+++ src/test/java/starter/navigation/WikipediaHomePage.java
@@ -3,5 +3,5 @@ package starter.navigation;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl("https://wikipedia.org")
+@DefaultUrl("https://dmuat.lta.gov.sg")
 public class WikipediaHomePage extends PageObject {}
